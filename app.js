92bcb167e52cb7cd685b443b1fe8f151664721cc
salvinanto7 <salvinanto7@gmail.com>
2022-07-16 15:31:39 +0530

    database connected

--- app.js
+++ app.js
@@ -7,21 +7,36 @@ var logger = require('morgan');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars');
+//var fileUpload = require('express-fileupload');
+var busboyparser=require('busboy-body-parser');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var forms = multer();
 var app = express();
-
+var db = require('./config/connection')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
+app.use(busboyparser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+db.connect((err)=>{
+  if(err) console.log('connection error'+err);
+  else console.log('Database Connected Successfully');
+});
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
+//app.use(fileUpload());
 
+//app.use(bodyParser.json());
+//app.use(forms.array()); 
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(multer().array());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/admin.js
+++ routes/admin.js
@@ -39,4 +39,9 @@ router.get('/', function(req, res, next) {
 router.get('/add-products',(req,res)=>{
   res.render('admin/add-products',{admin:true});
 });
+
+router.post('/add-products',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.image);
+});
 module.exports = router;
